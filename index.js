91da91ba8ab3003de316b1f65c573d3c2d85263f
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-29 13:32:06 -0500

    what did i just do

--- index.js
+++ index.js
@@ -2,22 +2,25 @@
 
 async function start (e, options) {
   let clicked = false
-  const opts = options || `width=200,height=20, left=${e.screenX - 200}, top=${e.screenY - 100}`
+  const opts = options || `width=200,height=20, left=${e.screenX - 20}, top=${e.screenY - 100}`
   const win = open('about:blank', '', opts)
   win.document.body.style.margin = '0'
   win.document.body.innerHTML = `
   <input style="display: none" type="text" autofocus></input>
-<a style="height: 100%; width: 100%; margin: 0; user-focus: none; display: block; background: black; color: white; text-align: center;" href="https://twitter.com/intent/tweet?text=@Jack5O79%20I%20like%20your%20website">Tweet to @Jack5o79</a>
+<a style="height: 100%; width: 100%; user-focus: none; display: block; background: black; color: white; text-align: center;" href="https://twitter.com/intent/tweet?text=@Jack5O79%20I%20like%20your%20website">Tweet to @Jack5o79</a>
 `
   win.focus()
+  win.document.querySelector('input').focus()
   win.document.title = document.title
-  win.document.body.addEventListener('mousemove', e => {
+  win.addEventListener('mousemove', e => {
+    win.focus()
+    win.document.querySelector('input').focus()
     if (!clicked) {
       win.resizeTo(200, 200)
       win.moveTo(e.screenX - 100, e.screenY - 100)
-      win.document.querySelector('input').focus()
     }
   })
+
   win.document.querySelector('a').addEventListener('click', e => {
     win.document.querySelector('input').focus()
     clicked = true
